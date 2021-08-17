b29c69055192d665186c73318d57ab447e998347
talesCPV <tales@flexibus.com.br>
2021-08-17 10:36:39 -0300

    by_script -> ter ago 17 10:36:39 -03 2021

--- commit.sh
+++ commit.sh
@@ -4,13 +4,13 @@
 
 now=$(date)
 
-curl -H "Authorization: token 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" https://github.com/talesCPV/Nes_CHR.git
+curl -H https://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:x-oauth-basic@github.com/talesCPV/Nes_CHR.git
 
 git init
 
 git add *
 
-git remote add origin "https://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:x-oauth-basic@github.com/talesCPV/Nes_CHR.git"
+git remote add origin "https://github.com/talesCPV/Nes_CHR.git"
 
 git commit -m "by_script -> ${now}"
 
